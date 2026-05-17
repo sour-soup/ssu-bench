bd7e3a2da9dd94e2a9cef4efb243cb2060ccd609
s.utaliev <s.utaliev@tbank.ru>
2026-05-18 00:40:28 +0400

    fix: add amount column to payment entity

--- src/main/java/org/soup/ssu/bench/repository/PaymentRepository.java
+++ src/main/java/org/soup/ssu/bench/repository/PaymentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import static org.soup.ssu.bench.repository.entity.PaymentEntity.AMOUNT_COL;
 import static org.soup.ssu.bench.repository.entity.PaymentEntity.PAYMENT_ROW_MAPPER;
 import static org.soup.ssu.bench.repository.entity.PaymentEntity.RECEIVER_ID_COL;
 import static org.soup.ssu.bench.repository.entity.PaymentEntity.SENDER_ID_COL;
@@ -17,9 +18,9 @@ import static org.soup.ssu.bench.repository.entity.PaymentEntity.TYPE_COL;
 public class PaymentRepository {
 
     private static final String SQL_CREATE_PAYMENT = """
-            INSERT INTO payments (task_id, type, sender_id, receiver_id, created_at, updated_at)
-            VALUES(:task_id, :type, :sender_id, :receiver_id, now(), now())
-            RETURNING id, task_id, type, sender_id, receiver_id;
+            INSERT INTO payments (task_id, amount, type, sender_id, receiver_id, created_at, updated_at)
+            VALUES(:task_id, :amount, :type, :sender_id, :receiver_id, now(), now())
+            RETURNING id, amount, task_id, type, sender_id, receiver_id;
         """;
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
@@ -27,6 +28,7 @@ public class PaymentRepository {
     public PaymentEntity createPayment(PaymentEntity paymentEntity) {
         MapSqlParameterSource params = new MapSqlParameterSource()
             .addValue(TASK_ID_COL, paymentEntity.taskId())
+            .addValue(AMOUNT_COL, paymentEntity.amount())
             .addValue(TYPE_COL, paymentEntity.type())
             .addValue(SENDER_ID_COL, paymentEntity.senderId())
             .addValue(RECEIVER_ID_COL, paymentEntity.receiverId());
--- src/main/java/org/soup/ssu/bench/repository/entity/PaymentEntity.java
+++ src/main/java/org/soup/ssu/bench/repository/entity/PaymentEntity.java
@@ -14,18 +14,21 @@ public record PaymentEntity(BigInteger id,
                             BigInteger taskId,
                             BigInteger senderId,
                             BigInteger receiverId,
+                            BigInteger amount,
                             String type) {
 
     public static final String TASK_ID_COL = "task_id";
     public static final String SENDER_ID_COL = "sender_id";
     public static final String RECEIVER_ID_COL = "receiver_id";
     public static final String TYPE_COL = "type";
+    public static final String AMOUNT_COL = "amount";
 
     public static final RowMapper<PaymentEntity> PAYMENT_ROW_MAPPER = (rs, rowNum) -> PaymentEntity.builder()
         .id(BigInteger.valueOf(rs.getLong(ID_COL)))
         .taskId(BigInteger.valueOf(rs.getLong(TASK_ID_COL)))
         .senderId(BigInteger.valueOf(rs.getLong(SENDER_ID_COL)))
         .receiverId(BigInteger.valueOf(rs.getLong(RECEIVER_ID_COL)))
+        .amount(BigInteger.valueOf(rs.getLong(AMOUNT_COL)))
         .type(rs.getString(TYPE_COL))
         .build();
 }
